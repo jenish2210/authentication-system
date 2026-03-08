1f9bed6aaf30339d90ec6830f717d6c98325f5ff
Jenish <jenisonjenish22@gmail.com>
2026-03-08 12:06:22 +0530

    my sql update

--- php/db.php
+++ php/db.php
@@ -1,14 +1,15 @@
 <?php
 
-$host = "localhost";
-$user = "root";
-$password = "";
-$db = "intern";
+$host = getenv("bpevpjyiy4lpti1rrebf-mysql.services.clever-cloud.com");
+$user = getenv("uch9w3mg0espbueg");
+$password = getenv("gknVJbbAfXmsK1vkZ26m");
+$dbname = getenv("bpevpjyiy4lpti1rrebf");
+$port = getenv("3306");
 
-$conn = new mysqli($host,$user,$password,$db);
+$conn = new mysqli($host,$user,$password,$dbname,$port);
 
-if($conn->connect_error){
-die("DB connection failed");
+if ($conn->connect_error) {
+    die("Database connection failed: " . $conn->connect_error);
 }
 
 ?>
