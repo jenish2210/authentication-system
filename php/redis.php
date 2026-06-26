7fa48742d8a242bb28c777b651e2edb7ea192662
jenison v <120499819+jenish2210@users.noreply.github.com>
2026-06-26 12:17:47 +0530

    Improve error handling for Redis connection
    Handle Redis connection errors more gracefully.

--- php/redis.php
+++ php/redis.php
@@ -1,13 +1,11 @@
 <?php
+
 require '../vendor/autoload.php';
 
 $url = getenv("REDIS_URL");
 
 try {
     $redis = new Predis\Client($url);
-    $redis->set('test', 'hello');
-    echo $redis->get('test');
-    exit;
 } catch (Exception $e) {
-    die($e->getMessage());
+    die("Redis Error: " . $e->getMessage());
 }
